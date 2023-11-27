28f1c0db1eed8d733b0545fb115de04622943564
ruloalv <ruloalv@gmail.com>
2023-11-27 18:36:53 +0000

    index connected

--- appetit2/settings.py
+++ appetit2/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = "appetit2.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
