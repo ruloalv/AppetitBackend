334f55cb7acf5892ec06c6b4c82b7781395c0473
ruloalv <ruloalv@gmail.com>
2023-12-05 21:51:33 +0000

    change database info

--- appetit2/settings.py
+++ appetit2/settings.py
@@ -85,9 +85,19 @@ WSGI_APPLICATION = "appetit2.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
+    
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "roa23$db_comments",
+        "USER": "roa23",
+        "PASSWORD": "appetitdb",
+        "HOST": "roa23.mysql.pythonanywhere-services.com",
+        "PORT": "3306",
     }
 }
 
