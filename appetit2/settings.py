be49fbb0e2c581897507bc1a4ebdb751187c32c4
ruloalv <ruloalv@gmail.com>
2023-12-05 22:16:26 +0000

    fix static root and staticfiles colectstatics

--- appetit2/settings.py
+++ appetit2/settings.py
@@ -137,9 +137,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [
-    BASE_DIR / "staticfiles"
+    BASE_DIR / "static"
 ]
 
 # Default primary key field type
